5f358f1f10a1c7265e836ea7c145046188aa8c42
MROS <yc1043@gmail.com>
2020-01-26 16:42:58 +0800

    增加少許未實作的宣告

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,1 +1,20 @@
 pub mod lexer;
+pub mod parser;
+
+struct Category {}
+
+struct Bond {}
+
+struct Bonds {}
+
+struct Article {}
+
+// 一個板的發文規則
+struct Board {}
+
+impl Board {
+    // 判斷一篇文章能否在本板發表
+    fn can_publish(&self, article: &Article) -> bool {
+        unimplemented!();
+    }
+}
